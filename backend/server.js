677ad8da43682049be0a6b02be7cc91e2d3dc5a8
maryjonachua <maryjonaaa@gmail.com>
2023-08-12 18:16:33 +0800

    4th

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,14 @@
 const express = require("express")
 const dotenv = require("dotenv")
-const cors = require("cors")
+
 const chats = require("./data/data")
 
 const app = express()
 
 dotenv.config()
 
-app.use(cors());
 
-// Middleware to check Accept header for JSON
-const ensureJsonContentType = (req, res, next) => {
-    const acceptHeader = req.get('Accept');
-    
-    // If the Accept header indicates XML or other formats, handle the request
-    if (acceptHeader && acceptHeader !== 'application/json') {
-      return res.status(406).json({ error: 'Unsupported content type requested' });
-    }
-    
-    next(); // Proceed to the next middleware
-  };
-  
+
 
 
 // request = req response = res
@@ -30,45 +18,29 @@ app.get('/',(req,res)=>{
 })
 
 
-//original code
-// app.get('/api/chat',(req,res)=>{
-//     res.send(chats) 
 
-// })
-
-
-// frm chat gpt
 app.get('/api/chat',(req,res)=>{
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).send(chats) 
-    
+    res.send(chats) 
+
 })
 
+
+
+
 //original code
-// app.get('/api/chat/:id',(req,res)=>{
-//     // getting the param id from the request id
-//     //console.log(req.params.id)
-
-//     // find in data array all single data
-//     const singleChat = chats.find(c=>c._id === req.params.id)
-
-//     // send to user by response - res
-//     res.send(singleChat)
-
-// })
-
-
-// frm chat gpt
 app.get('/api/chat/:id',(req,res)=>{
+    // getting the param id from the request id
+    //console.log(req.params.id)
+
+    // find in data array all single data
     const singleChat = chats.find(c=>c._id === req.params.id)
 
-    if (!singleChat) {
-        return res.status(404).json({ error: 'Chat not found' });
-      }
-
-      res.status(200).json(singleChat)
+    // send to user by response - res
+    res.send(singleChat)
 
 })
+
+
 
 
 
@@ -79,9 +51,4 @@ const PORT = process.env.PORT || 5000
 // cors
 app.listen(PORT, () => console.log(`Server Started on Port ${PORT}`))
 
-const corsOptions = {
-    origin: 'http://localhost:5000', // Replace with your React app's URL
-  };
-  
-  app.use(cors(corsOptions));
   
