d564d16c4d53aa0365c52be39d086cc35565a61b
maryjonachua <maryjonaaa@gmail.com>
2023-08-12 13:59:25 +0800

    Creating APIs with Express JS

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,28 @@
 const express = require("express")
+const chats = require("./data/data")
 
 const app = express()
 
 // request = req response = res
 app.get('/',(req,res)=>{
     res.send('API is Running') 
+
+})
+
+app.get('/api/chat',(req,res)=>{
+    res.send(chats) 
+
+})
+
+app.get('/api/chat/:id',(req,res)=>{
+    // getting the param id from the request id
+    //console.log(req.params.id)
+
+    // find in data array all single data
+    const singleChat = chats.find(c=>c._id === req.params.id)
+
+    // send to user by response - res
+    res.send(singleChat)
 
 })
 
