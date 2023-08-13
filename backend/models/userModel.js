80a7b0afc8d822c6213a87e39114f832621a98f1
maryjonachua <maryjonaaa@gmail.com>
2023-08-13 11:46:39 +0800

    Password Encrypt w/ Bcrypt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const userSchema = mongoose.Schema({
     name:{type:String, required:true},
@@ -7,6 +8,22 @@ const userSchema = mongoose.Schema({
     pic:{type:String, default:'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg'}
 },{
     timestamps:true
+})
+
+// match user password
+userSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password)
+}
+
+
+// before saving to the Db it will encrypt the password first
+userSchema.pre('save',async function(next){
+    if(!this.isModified){
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password,salt)
 })
 
 const User = mongoose.model("User",userSchema)
