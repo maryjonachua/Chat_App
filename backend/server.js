2b58200370228d3f7f92314d7d6f7bc1e7dccb25
maryjonachua <maryjonaaa@gmail.com>
2023-08-14 20:55:22 +0800

    laaaaast

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const userRoutes = require('./routes/userRoutes')
 const chatRoutes = require('./routes/chatRoutes')
 const messageRoutes = require('./routes/messageRoutes')
 const { notFound, errorHandler } = require("./middleware/errorMiddleware")
-
+const path = require("path");
 
 
 
@@ -35,6 +35,26 @@ app.get('/',(req,res)=>{
 app.use('/api/user',userRoutes)
 app.use('/api/chat',chatRoutes)
 app.use('/api/message',messageRoutes)
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment-------------------------
+
+
 
 // error handling middleware
 app.use(notFound)
